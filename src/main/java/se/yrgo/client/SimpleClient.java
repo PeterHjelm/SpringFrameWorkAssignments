e99e6a3d4f4856356401750c9494f6d70fc319ab
PeterHjelm <peter.hjelm1@gmail.com>
2025-05-04 18:59:06 +0200

    first and unfortunately the only commit, finished everything locally

--- src/main/java/se/yrgo/client/SimpleClient.java
+++ src/main/java/se/yrgo/client/SimpleClient.java
@@ -1,4 +1,9 @@
 package se.yrgo.client;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.GregorianCalendar;
+import java.util.List;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import se.yrgo.domain.Action;
 import se.yrgo.domain.Call;
@@ -8,48 +13,37 @@ import se.yrgo.services.customers.CustomerManagementService;
 import se.yrgo.services.customers.CustomerNotFoundException;
 import se.yrgo.services.diary.DiaryManagementService;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.GregorianCalendar;
-import java.util.List;
-
 public class SimpleClient {
 
     public static void main(String[] args) {
-
-     ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("application.xml");
-        CustomerManagementService service = container.getBean(CustomerManagementService.class);
+        ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("application.xml");
 
         CustomerManagementService customerService = container.getBean(CustomerManagementService.class);
+        CallHandlingService callService = container.getBean(CallHandlingService.class);
         DiaryManagementService diaryService = container.getBean(DiaryManagementService.class);
 
-        CallHandlingService callService = container.getBean(CallHandlingService.class);
+        customerService.newCustomer(new Customer("CS03939", "Acme", "Good Customer"));
 
-        Call newCall = new Call("Dom called from Twin Peaks Company");
-        Action action1 = new Action ("Call back Dom as soon as possible for feedback",
-                new GregorianCalendar(2019,12,10), "user");
-        Action action2 = new Action ("Check if Dom called again",
-                new GregorianCalendar(2019,12,11), "user");
-        List<Action>actions = new ArrayList<Action>();
+        Call newCall = new Call("Larry Wall called from Acme Corp");
+        Action action1 = new Action("Call back Larry to ask how things are going", new GregorianCalendar(2016, 0, 0), "rac");
+        Action action2 = new Action("Check our sales dept to make sure Larry is being tracked", new GregorianCalendar(2016, 0, 0), "rac");
+
+        List<Action> actions = new ArrayList<Action>();
         actions.add(action1);
         actions.add(action2);
 
-        try {
-            callService.recordCall("NV10", newCall, actions);
-        }catch(CustomerNotFoundException e) {
-            System.err.println("This customer does not exist.");
+        try{
+            callService.recordCall("CS03939", newCall, actions);
+        }catch (CustomerNotFoundException e){
+            System.out.println("That customer doesn't exist");
         }
 
-        System.out.println("Here are the actions:");
-        Collection<Action> incompleteActions = diaryService.getAllIncompleteActions("user");
-        for(Action action:incompleteActions) {
-            System.out.println(action);
+        System.out.println("Here are the outstanding actions:");
+        Collection<Action> incompleteActions = diaryService.getAllIncompleteActions("rac");
+        for (Action next: incompleteActions){
+            System.out.println(next);
         }
-        List<Customer> customers = service.getAllCustomers();
-        for (Customer customer : customers) {
-            System.out.println(customer);
 
-            container.close();
-        }
+        container.close();
     }
 }
