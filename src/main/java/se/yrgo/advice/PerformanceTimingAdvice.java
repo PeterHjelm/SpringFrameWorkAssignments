ea110657a5652f3f4279ecd882d4e76bab05fdea
PeterHjelm <peter.hjelm1@gmail.com>
2025-05-14 13:39:19 +0200

    making small changes in CustomerDaoJdbcTemplateImpl class and finishing the assignment

--- src/main/java/se/yrgo/advice/PerformanceTimingAdvice.java
+++ src/main/java/se/yrgo/advice/PerformanceTimingAdvice.java
@@ -10,8 +10,8 @@ public class PerformanceTimingAdvice {
         } finally {
             long end = System.currentTimeMillis();
             long result = end - start;
-            System.out.println("This is the time taken for method " + pjp.getSignature().getName() +
-                    " from the class " + pjp.getSignature().getDeclaringTypeName() + " took " + result + "ms");
+            System.out.println("Time taken for method " + pjp.getSignature().getName() +
+                    " from the class " + pjp.getSignature().getDeclaringTypeName() + " to process took " + result + " ms");
         }
     }
 
